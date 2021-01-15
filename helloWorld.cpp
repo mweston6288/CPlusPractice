73b5dbd5c286d101bf029664677ae548ed287409
felixcloud6288 <mweston6288@knights.ucf.edu>
2021-01-15 12:50:47 -0500

    Added more notes that explain how to use library without using namespace

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,9 +1,14 @@
 // Use g++ file.cpp to compile. Default executable is a.out
 
-#include <iostream>
+#include <iostream> // input/output library
 using namespace std;
 
 int main(){
 	cout << "Hello World!"; // Does not include newline break
+	/*
+	without using namespace std;
+	std::cout << "Hello World!";
+
+	*/
 	return 0; //optional for main
 }

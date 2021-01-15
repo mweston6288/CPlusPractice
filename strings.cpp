42df640524a4f1ecdfddb96e7f2c37a756038139
felixcloud6288 <mweston6288@knights.ucf.edu>
2021-01-15 13:32:32 -0500

    More string practice and notes

--- strings.cpp
+++ strings.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
-
+/*
+	without using namespace std, declaring a string requires std::string
+*/
 int main(){
 	string firstName = "John ";
 	string lastName = "Doe";
@@ -11,4 +13,7 @@ int main(){
 	cout << fullName[2]<<"\n"; // access chars as array
 	fullName[0] = 'Q'; // change a char in the string
 	cout << fullName << "\n";
+	// cin can get a string but only the first word. SPaces are terminators
+	getline(cin, fullName); // this will get a line rather than a single word
+	cout << fullName;
 }

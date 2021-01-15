3a53e68ad478fc413ab2aa3b27d17b346c20444a
felixcloud6288 <mweston6288@knights.ucf.edu>
2021-01-15 13:01:08 -0500

    Working on variable declarations

---
+++ variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x, z; // can declare multiple of same variable
+	double y;
+	char c;
+	string s; // string are default variables in C++;
+	bool b;
+	x = 50;
+	z = 5;
+	y = 4.7;
+	c = 'x';
+	s = "Hello World!";
+	b = true;
+	cout << x << " " << s<< endl; // use << when transitioning between string literal and variable
+	cout << 2*z; // can do math
+}

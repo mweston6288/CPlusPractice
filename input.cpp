c0d07847ad0bb7c4ccce5a285968cc1a7a6e2b9b
felixcloud6288 <mweston6288@knights.ucf.edu>
2021-01-15 13:10:36 -0500

    Made a very basic adder with cin

--- input.cpp
+++ input.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 
 int main(){
-	int x;
+	int x, y;
 	cout << "Type a number\n";
 	cin >> x; // take user input; does not confirm type match
 	cout << x <<"\n"; 
+	cout << "type another number\n";
+	cin >> y;
+	cout << "Smm: "<< x+y<<"\n";
 }

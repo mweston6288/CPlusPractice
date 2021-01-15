78f2505aa1b3d702c2ef926e97e60ed43a1d07ab
felixcloud6288 <mweston6288@knights.ucf.edu>
2021-01-15 12:46:27 -0500

    Made the initial helloWorld file and wrotes some notes about it

---
+++ helloWorld.cpp
@@ -0,0 +1,9 @@
+// Use g++ file.cpp to compile. Default executable is a.out
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Hello World!"; // Does not include newline break
+	return 0; //optional for main
+}

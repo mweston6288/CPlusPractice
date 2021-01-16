59fca1d029366822c0eedcac448dcc2d5300e966
felixcloud6288 <mweston6288@knights.ucf.edu>
2021-01-16 08:36:07 -0500

    Fiddled with ternaries

--- conditions.cpp
+++ conditions.cpp
@@ -3,9 +3,15 @@ using namespace std;
 
 int main(){
 	bool x = false;
+	bool y = true;
 	if (x){
 		cout << "x is true\n";
-	} else{
+	} else if (y){
+		cout << "y is true\n";
+	}
+	else{
 		cout << "x is false\n";
 	}
+	// cout does not work properly with ternaries without parenthesis 
+	cout << (y ? "y is true\n" : "y is false\n");
 }

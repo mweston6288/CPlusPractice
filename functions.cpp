cb08592413f44a306811f706ec41cbcb4a0505ed
felixcloud6288 <mweston6288@knights.ucf.edu>
2021-01-17 10:44:01 -0500

    Practicing functions

---
+++ functions.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// each function must be declared before it is ever used
+void function(){
+	cout <<"Entered function\n";
+}
+
+int main(){
+	function();
+}

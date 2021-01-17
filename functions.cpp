a653cb2aa18888f350e6f2f5eec9757f424ca076
felixcloud6288 <mweston6288@knights.ucf.edu>
2021-01-17 10:46:27 -0500

    add functions with parameters

--- functions.cpp
+++ functions.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void function2(); // function signature
 // each function must be declared before it is ever used
 void function(){
 	cout <<"Entered function\n";
 }
 
+void printName(string x){
+	cout << x<<"\n";
+}
 int main(){
 	function();
+	function2();
+	printName("Larry");
+}
+
+void function2()
+{
+	cout << "Entered function\n";
 }

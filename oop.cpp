4c7b6b73419e48a000de16357dde0a5122bc5552
felixcloud6288 <mweston6288@knights.ucf.edu>
2021-01-18 10:21:04 -0500

    Practice with access and encapsulation

--- oop.cpp
+++ oop.cpp
@@ -14,6 +14,14 @@ class MyClass{ //declare class like in java
 			num = x;
 			myString = "Hello World!";
 		}
+		void setY(int y){
+			this->y = y;
+		}
+		int getY(){
+			return y;
+		}
+	private: // cannot be accessed outside the class
+		int y;
 };
 // how to define a method outside the class
 void MyClass::method2(){
@@ -25,4 +33,10 @@ int main(){
 
 	obj.method();
 	obj.method2();
+	obj.setY(20);
+	cout << obj.getY()<<endl;
 }
+
+/*
+	By default, all class elements are private
+*/
